788a60556b30efdfd0c7493933deccd28a9d409a
Tate777331 <estate.keldishvili.1@btu.edu.ge>
2022-05-11 22:46:48 +0400

    Homework6

---
+++ app/src/main/java/com/example/hw6/ImageFragment.kt
@@ -0,0 +1,48 @@
+package com.example.hw6
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hw6.R
+
+
+class ImageFragment : Fragment(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater!!.inflate(R.layout.layout_image_fragment,container,false)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
---
+++ app/src/main/java/com/example/hw6/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.hw6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button1.setOnClickListener {
+
+            val textFragment = TextFragment()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+
+            transaction.replace(R.id.fragment_container,textFragment)
+            transaction.addToBackStack(null)
+
+
+            transaction.commit()
+        }
+
+
+        button2.setOnClickListener {
+
+            val imageFragment = ImageFragment()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+
+            transaction.replace(R.id.fragment_container,imageFragment)
+            transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
+    }
+}
